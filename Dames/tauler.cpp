f6807dd074f6df1562cdcdf3d3090f977d242702
Enric Senata Gal-Boguñá <1749879@uab.cat>
2025-04-10 12:20:16 +0200

    Posicio i tauler

---
+++ Dames/tauler.cpp
@@ -0,0 +1,76 @@
+#include "tauler.h"	
+
+
+
+void inicialitzaTauler(char tauler[N_FILES][N_COLUMNES]) {
+    for (int i = 0; i < N_FILES; i++) 
+    {
+        for (int j = 0; j < N_COLUMNES; j++) {
+            tauler[i][j] = ' ';
+        }
+    }
+}
+
+void llegeixTauler(const string& nomFitxer, char tauler[N_FILES][N_COLUMNES])
+{
+    inicialitzaTauler(tauler);
+
+    ifstream fitxer;
+    fitxer.open(nomFitxer);
+    if (fitxer.is_open()) 
+    {
+        char figura;
+        Posicio posicio;
+
+        fitxer >> figura;
+        fitxer >> posicio;
+
+        tauler[posicio.getFila()][posicio.getColumna()] = figura;
+
+        while (!fitxer.eof()) {
+
+            fitxer >> figura;
+            fitxer >> posicio;
+
+            tauler[posicio.getFila()][posicio.getColumna()] = figura;
+
+            
+        }
+        
+
+        fitxer.close();
+
+    }
+    
+
+}
+
+void escriuTauler(const string& nomFitxer, char tauler[N_FILES][N_COLUMNES])
+{
+    ofstream fitxer;
+    fitxer.open(nomFitxer);
+
+    if (fitxer.is_open())
+    {
+        string tipusFitxa;
+
+        for (int i = 0; i < N_FILES; i++)
+        {
+            for (int j = 0; j < N_COLUMNES; j++) 
+            {
+                if (tauler[i][j] != ' ') 
+                {
+                    Posicio posicio(i, j);
+                    posicio.toString();
+                    fitxer << tauler[i][j] << " ";
+                    fitxer << posicio << endl;
+                }
+                
+
+            }
+        }
+
+        fitxer.close();
+    }
+
+}
